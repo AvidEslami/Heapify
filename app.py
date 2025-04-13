149d8450cbd798a2e6c309f7aecec3b6ba6b96e3
AvidEslami <avideslami@gmail.com>
2025-04-12 22:08:40 -0400

    Initialize data dir

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os
+# Create an empty ./data/ directory if it doesn't exist
+if not os.path.exists('./data'):
+    os.makedirs('./data')
 
 graph_view = st.Page("pages/graph_view.py", title="Graph View", icon="🌳")
 node_view = st.Page("pages/node_view.py", title="Node View", icon="📖")
